a953d41e0ddb730762dba689d1c9b6d08f98ef6f
Zvika Ben-Haim <zvikabh@gmail.com>
2025-07-11 19:03:47 +0300

    extract celestial_mechanics.py

---
+++ src/celestial_mechanics.py
@@ -0,0 +1,69 @@
+"""Classes and functions for computing motion of celestial bodies."""
+
+import dataclasses
+import math
+from typing import Sequence
+
+import pygame as pg
+
+
+# Physical constants
+UNIVERSE_RECT = pg.Rect(0, 0, 1280*1e6, 1024*1e6)
+GRAVITATIONAL_CONSTANT = 6e-11
+
+
+@dataclasses.dataclass
+class Vector:
+  x: float
+  y: float
+
+  def __sub__(self, other: 'Vector') -> 'Vector':
+    return Vector(self.x - other.x, self.y - other.y)
+  
+  def __add__(self, other: 'Vector') -> 'Vector':
+    return Vector(self.x + other.x, self.y + other.y)
+
+  def __mul__(self, scalar: float):
+    return Vector(self.x * scalar, self.y * scalar)
+  
+  def __rmul__(self, scalar: float):
+    return Vector(self.x * scalar, self.y * scalar)
+  
+  def __truediv__(self, scalar):
+    return Vector(self.x / scalar, self.y / scalar)
+
+  @property
+  def squared_norm(self) -> float:
+    return self.x * self.x + self.y * self.y
+  
+  @property
+  def norm(self) -> float:
+    return math.sqrt(self.squared_norm)
+
+
+@dataclasses.dataclass
+class SpaceObject:
+  mass: float
+  position: Vector  # meters
+  velocity: Vector  # meters/sec
+
+
+@dataclasses.dataclass
+class CelestialBody(SpaceObject):
+  name: str
+  radius: float  # meters
+  color: tuple[int, int, int]
+
+
+def update_positions(objects: Sequence[SpaceObject], time_step: float) -> None:
+  for i, this_obj in enumerate(objects):
+    total_accel = Vector(0., 0.)
+    for j, other_obj in enumerate(objects):
+      if i == j:
+        continue
+      delta: Vector = other_obj.position - this_obj.position
+      cur_accel = (GRAVITATIONAL_CONSTANT * other_obj.mass / delta.norm / delta.squared_norm) * delta
+      total_accel = total_accel + cur_accel
+    this_obj.velocity = this_obj.velocity + total_accel * time_step
+    this_obj.position = this_obj.position + this_obj.velocity * time_step
+
--- src/space_rl_main.py
+++ src/space_rl_main.py
@@ -1,79 +1,22 @@
 import dataclasses
 import math
+import os
 from typing import Sequence
 
 import pygame as pg
 
+import celestial_mechanics as cm
+
 # Display constants
-SCREEN_RECT = pg.Rect(0, 0, 1280, 1024)
 SCALE = 1e6  # meters per pixel
+SCREEN_RECT = pg.Rect(0, 0, cm.UNIVERSE_RECT.width/SCALE, cm.UNIVERSE_RECT.height/SCALE)
 BACKGROUND_COLOR = (0, 0, 0)
 SEC_PER_FRAME = 0.04
-
-# Physical constants
-UNIVERSE_RECT = pg.Rect(0, 0, SCREEN_RECT.width * SCALE, SCREEN_RECT.height * SCALE)
-GRAVITATIONAL_CONSTANT = 6e-11
-
-
-@dataclasses.dataclass
-class Vector:
-  x: float
-  y: float
-
-  def __sub__(self, other: 'Vector') -> 'Vector':
-    return Vector(self.x - other.x, self.y - other.y)
-  
-  def __add__(self, other: 'Vector') -> 'Vector':
-    return Vector(self.x + other.x, self.y + other.y)
-
-  def __mul__(self, scalar: float):
-    return Vector(self.x * scalar, self.y * scalar)
-  
-  def __rmul__(self, scalar: float):
-    return Vector(self.x * scalar, self.y * scalar)
-  
-  def __truediv__(self, scalar):
-    return Vector(self.x / scalar, self.y / scalar)
-
-  @property
-  def squared_norm(self) -> float:
-    return self.x * self.x + self.y * self.y
-  
-  @property
-  def norm(self) -> float:
-    return math.sqrt(self.squared_norm)
-
-
-@dataclasses.dataclass
-class SpaceObject:
-  mass: float
-  position: Vector  # meters
-  velocity: Vector  # meters/sec
-
-
-@dataclasses.dataclass
-class CelestialBody(SpaceObject):
-  name: str
-  radius: float  # meters
-  color: tuple[int, int, int]
-
-
-def update_positions(objects: Sequence[SpaceObject]) -> None:
-  for i, this_obj in enumerate(objects):
-    total_accel = Vector(0., 0.)
-    for j, other_obj in enumerate(objects):
-      if i == j:
-        continue
-      delta: Vector = other_obj.position - this_obj.position
-      cur_accel = (GRAVITATIONAL_CONSTANT * other_obj.mass / delta.norm / delta.squared_norm) * delta
-      total_accel = total_accel + cur_accel
-    this_obj.velocity = this_obj.velocity + total_accel * SEC_PER_FRAME
-    this_obj.position = this_obj.position + this_obj.velocity * SEC_PER_FRAME
 
 
 class CelestialBodySprite(pg.sprite.Sprite):
 
-  def __init__(self, body: CelestialBody, sprite_groups: Sequence[pg.sprite.Group]):
+  def __init__(self, body: cm.CelestialBody, sprite_groups: Sequence[pg.sprite.Group]):
     super().__init__(*sprite_groups)
     self.body = body
     diameter_pixels = int(self.body.radius * 2 / SCALE + 1)
@@ -103,18 +46,18 @@ def main():
   clock = pg.time.Clock()
   all_sprites = pg.sprite.Group()
   celestial_bodies = [
-    CelestialBody(
+    cm.CelestialBody(
       mass=1.5e34, 
-      position=Vector(UNIVERSE_RECT.width / 2, UNIVERSE_RECT.height / 2),
-      velocity=Vector(0, 0.),
+      position=cm.Vector(cm.UNIVERSE_RECT.width / 2, cm.UNIVERSE_RECT.height / 2),
+      velocity=cm.Vector(0, 0.),
       name='planet',
       radius=SCALE*30,
       color=(0, 100, 255),
     ),
-    CelestialBody(
+    cm.CelestialBody(
       mass=3e31,
-      position=Vector(UNIVERSE_RECT.width / 2, UNIVERSE_RECT.height * 0.2),
-      velocity=Vector(5e7, 0.),
+      position=cm.Vector(cm.UNIVERSE_RECT.width / 2, cm.UNIVERSE_RECT.height * 0.2),
+      velocity=cm.Vector(5e7, 0.),
       name='moon',
       radius=SCALE*10,
       color=(128, 128, 128),
@@ -136,7 +79,7 @@ def main():
       if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
         return
       
-    update_positions(celestial_bodies)
+    cm.update_positions(celestial_bodies, time_step=SEC_PER_FRAME)
     all_sprites.update()
     screen.fill(BACKGROUND_COLOR)
     all_sprites.draw(screen)
